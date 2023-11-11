1dd25b894ca097eb3ff86726ed85c70507ba9881
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2023-11-11 10:08:14 -0800

    Week-5-Day-1

--- Week-5/Day-1/N-MinimizetheThickness.cpp
+++ Week-5/Day-1/N-MinimizetheThickness.cpp
@@ -16,27 +16,26 @@ int main(){
             x=prefixSum[i];
         }
 
+    vector<int> tempResult;
     vector<int> finalResult;
 
         for(int i=1;i<=n;i++){
             if(prefixSum[n]%prefixSum[i]==0){
-                int val=prefixSum[i],mul=2,preAns=i,l=0;
-                for(int k=i+1;k<=n;k++){
+                int val=prefixSum[i],mul=2,preAns=i,l=0, k=i+1;
+                for(k;k<=n;k++){
                     l++;
                     if(prefixSum[k]==val*mul){
                         preAns=max(preAns,l);
                         l=0;
                         mul++;
                     }
-                    i=k-1;
                 }
-                finalResult.push_back(preAns);
+               if(prefixSum[k-1]==val*(mul-1)) tempResult.push_back(preAns);
             }
+            sort(tempResult.begin(),tempResult.end());   
         }
 
-        sort(finalResult.begin(),finalResult.end());
-        for(auto vl:finalResult) cout<<vl<<" ";
-         cout<<finalResult[0]<<'\n';
+       cout<<tempResult[0]<<'\n';
     }
 
     return 0;
